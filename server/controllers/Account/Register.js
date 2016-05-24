e6c96dbfdf9526e20b4077719303ab43faf78926
jamesmartindavies <jamesmd@plumpdigital.co.uk>
2016-05-24 17:05:23 +0100

    Prevent posting registration when signed in to session.

--- server/controllers/Account/Register.js
+++ server/controllers/Account/Register.js
@@ -17,6 +17,10 @@ export const Register = {
 		path: '/register',
 		method: 'post',
 		controller(req, res, next) {
+			if (req.isAuthenticated()) {
+				return res.redirect(`/account/${encodeURIComponent(req.user.username)}`);
+			}
+
 			User.register(new User({ username: req.body.username }), req.body.password, (err) => {
 				if (err) {
 					console.log('Error registering user.', err);
